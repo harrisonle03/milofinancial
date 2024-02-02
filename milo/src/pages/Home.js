bb7782f3185a8473cbc2bda07d0d28fe089e3466
hotharry123 <91809765+hotharry123@users.noreply.github.com>
2024-02-02 01:29:11 -0500

    Created the home page

--- milo/src/pages/Home.js
+++ milo/src/pages/Home.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import {
+  Card,
+  CardBody,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
+
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Milo Financial! To get started, please insert a CSV statement from your bank. Then let Milo do all the work and showcase your spending in a nice organized format for ya. BARK.</h1>
+    <div className="flex justify-center items-center h-[calc(100vh-2rem)] w-full bg-[#1F2833] p-8">
+
+      <div className="text-center">
+
+      <img src="https://upload.wikimedia.org/wikipedia/en/5/53/Shiba_Inu_coin_logo.png" alt="brand" className="h-25 w-25 mx-auto mb-6" />
+      
+        <Typography variant="h1" color="cyan" className="mb-6 style={{ fontWeight: 'bold' }}">
+          Welcome to Milo Personal <br></br>Financial Tool
+        </Typography>
+
+        <Typography variant="h3" color="white" className="mb-8">
+          To get started, please insert a CSV statement from your bank. Then let Milo do all the work and <br></br>showcase your spending in a nice organized format for ya. BARK.
+        </Typography>
+
+      <div className="mb-8">
+      <Button variant="gradient" color="cyan" className="px-20 py-7 text-lg">Get Started</Button>
+      </div>
+
+      </div>
+
+      <Footer></Footer>
+
     </div>
-  )
+  );
 }
